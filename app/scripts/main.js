f20b1ef1c2f1acd0449b7b3230012b0ae9f09131
luke olson <luukeout@gmail.com>
2014-05-15 18:19:42 -0400

    Beginning of Git repo page

---
+++ app/scripts/main.js
@@ -0,0 +1,25 @@
+
+
+// var goodMorningTemplate = _.template ($('.goodmorning-template').text);
+
+// var renderedTemplate = goodMorningTemplate({
+// 	person: "Mason",
+// 	feeling: "great",
+// });
+
+// console.log();
+
+// $('body').prepend(morningMason);
+
+
+// var nameTemplate = _.template ($('.name-template').text);
+
+// var renderedNameTag = nameTemplate({
+// 	person: "luke";
+// })
+
+// console.log(renderedNameTag);
+
+// How do I get info from a set of data into this template?
+
+// I think I need to 
